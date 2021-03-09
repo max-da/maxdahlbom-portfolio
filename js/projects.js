e978c8d4b52d4951e83dde5083866339ee4ae3ac
max-da <max.dahlbom@outlook.com>
2021-03-09 15:27:53 +0100

    before test

--- js/projects.js
+++ js/projects.js
@@ -1,27 +1,52 @@
 class project{
-    constructor(name, desc, image, gist){
+    constructor(name, desc, image, gist, path,imageNumber){
         this.name = name;
         this.desc = desc;
         this.image = image;
         this.gist = gist;
+        this.path = path;
+        this.imageNumber =  imageNumber;
     }
 }
-const project1 = new project("Todolist", "Mitt första projekt", "../todoJS.png","gist" )
+const project1 = new project("Todolist", "Mitt första projekt", "image1.png","gist", "../assets/webbshop/")
+const project2 = new project("Todolist", "Mitt första projekt", "image1.png","gist", "../assets/webbshop/")
 
-const projectArray = [project1]
+const projectArray = [project1,project2]
 
 $(function(){
     $.each(projectArray, (i,project)=> {
         let projectBox = $("<div>")
         .addClass("project-box")
-        .appendTo("#projects-list")
+        .appendTo("#projects-list");
       
-        let titleProject = $("<h3>")
+      
+   
+
+        let imageHandlerDiv = $("<div>")
+        .addClass("image-handler-div")
+        .appendTo(projectBox);
+        
+        let imageProject = $("<img>")
+        .attr("src", project.path + project.image)
+        .appendTo(imageHandlerDiv);
+
+        //projectBox.append(titleProject, imageHandlerDiv)
+        
+        let overlay = $("<div>")
+        .addClass("overlay")
+        .appendTo(imageHandlerDiv); 
+
+     let titleProject = $("<h3>")
         .attr("id","title-project")
         .html(project.name)
-        let imageProject = $("<img>")
-        .attr("src",project.image);
-        projectBox.append(titleProject, imageProject)
+        .appendTo(imageHandlerDiv); 
+
+        let desc = $("<span>")
+        .html(project.desc)
+        .appendTo(projectBox);
+    
+        
     })
 
 })
+
