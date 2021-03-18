c49c79c31eac1ccc6d3e4bd9072add18853ebe09
max-da <max.dahlbom@outlook.com>
2021-03-18 19:56:52 +0100

    update

--- js/projects.js
+++ js/projects.js
@@ -1,40 +1,62 @@
 class project {
-  constructor(name, desc, image, gist, path, imageNumber) {
+  constructor(name, desc, image,image2, image3, tools, path, gitlink,date) {
     this.name = name;
     this.desc = desc;
     this.image = image;
-    this.gist = gist;
+    this.image2 = image2;
+    this.image3 = image3;
+    this.tools = tools;
     this.path = path;
-    this.imageNumber = imageNumber;
+    this.gitlink = gitlink;
+    this.date = date;
+   
   }
 }
 const project1 = new project(
   "authorization",
+  "Det här är vår andra uppgift i backend-kursen, och ett av mina roligaste projekt hitintills. Uppgiften var att ta vårt tidigare projekt i kursen, och implementera authentication. Jag omstrukturerade projeketet efter MVC arkitektur, och la till inloggning och registrering av användare, som använder bcrypt för att hasha data innan användaren sparas i databasen. Jag använder också JWT för att spara användarens inloggning under en specifierad tid, för att slippa logga in på nytt varje gång man byter sida. Det går också att återställa sitt lösenord, vilket sköts med nodemailer. Här hade jag lite tid över så jag passade på att göra om stylingen från grunden, vilket gjorde mig betydligt nöjdare med resultatet.",
+  "image1.png",
+  "image2.png",
+  "image3.png",
   "HTML,SCSS, NODE.js & EJS",
-  "image1.png",
-  "gist",
-  "../assets/todo-back/"
-);
-const project2 = new project(
-  "webshop",
-  "HTML,SCSS,JQUERY",
-  "image1.png",
-  "gist",
-  "../assets/webbshop/"
+  "../assets/todo-back/",
+  "https://github.com/max-da/backendCourseTodoApp",
+  "4th march 2021"
 );
 const project3 = new project(
+  "webshop",
+  "Det här är ett grupprojekt från vår Javascriptkurs. Vi arbetade i team om tre, och jag hade det huvudsakliga ansvaret för JS-delen. Det var väldigt lärorikt att arbeta i grupp, särskilt med tanke på att vi då inte ännu lärt oss agila arbetssätt. Så det sätter verkligen i perspektiv hur användbart det är, då det stundvis var svårt att strukturera vårt arbeta utan något konkret tillvägagångssätt.  ",
+  "image1.png",
+  "image2.png",
+  "image3.png",
+  "HTML,SCSS,JQUERY",
+  "../assets/webbshop/",
+  "https://github.com/feliciatranberg/GruppArbeteWebbshop",
+  "23th dec 2020"
+
+);
+const project2 = new project(
   "todolist",
-  "HTML,SCSS,NODE.js & EJS",
+  "Det här är den första uppgiften från vår backend-kurs. Jag skapade en webserver med express, sparar data i mongoDB med Mongoose, och sköter eventuell front-end kod med EJS. Jag tycker verkligen om backend-kursen, jag har fått en helt annan inblick i hur en hemsida fungerar. Tyvärr hann jag inte riktigt lägga lika mycket krut på stylingen som jag hade velat, men i slutändan var det inte det som var uppgiftens fokus. ",
   "image1.PNG",
-  "gist",
-  "../assets/todoback/"
+  "nopic",
+  "nopic",
+  "HTML,SCSS,NODE.js & EJS",
+  "../assets/todoback/",
+  "https://github.com/max-da/Todoapp-backend",
+  "8th feb 2021"
+
 );
 const project4 = new project(
   "todolist",
-  "HTML,SCSS,JS & CSS",
+  "När jag ser tillbaka på dem här äldre projeketen blir jag både irriterad för att jag inte tycker att dem riktigt håller måttet, men också nöjd över att dem reflekterar att jag hela tiden utvecklas.Det här är mitt första projekt från vår Javascript-kurs, en enkel todo-list. Syftet med uppgiften var huvudsakligen att lära sig hur js-klasser fungerar, hur man renderar dem dynamiskt, och hur man hanterar data i localstorage.",
   "image1.PNG",
-  "gist",
-  "../assets/first/"
+  "nopic",
+  "nopic",
+  "HTML,JS & CSS",
+  "../assets/first/",
+  "https://github.com/max-da/todolistJsHomework",
+  "18th nov 2020"
 );
 
 const projectArray = [project1, project2, project3, project4];
@@ -68,7 +90,7 @@ $(function () {
       .appendTo(imageHandlerDiv);
 
     let descBox = $("<div>").addClass("desc-box").appendTo(projectBox);
-    $("<span>").html(project.desc).appendTo(descBox);
+    $("<span>").html(project.tools).appendTo(descBox);
   });
 });
 
--- js/specificProject.js
+++ js/specificProject.js
@@ -1,18 +1,87 @@
 $(function(){
     let project = getProduct();
-    let contentContainer =  $(".content-container");
+    render(project);
+
+  
+
+    
+})
+let x = 1;
+function render(project){
+  
+    let contentContainer =  $(".spec-content-container");
 
     let projectHeading = $("<h1>")
     .html(project.name)
     .appendTo(contentContainer)
 
-    let projectImage = $("<img>")
-    .attr("src", project.path + project.image)
+    let imageContainer = $("<div>")
+    .addClass("image-container")
     .appendTo(contentContainer)
 
-    
-})
 
+    let projectImage = $("<img>")
+    .addClass("image")
+    .attr("src", project.path + project.image)
+    .appendTo(imageContainer)
+
+    let textContainer = $("<div>")
+    .addClass("text-container")
+    .appendTo(contentContainer);
+
+    let gitDateContainer = $("<div>")
+    .addClass("git-date-container")
+    .appendTo(textContainer);
+    $("<a>")
+    .attr("href", project.gitlink)
+    .attr("target", "_blank")
+    .attr("rel","noopener noreferrer")
+    .addClass("fab fa-git")
+    .appendTo(gitDateContainer);
+    $("<span>")
+    .html(project.date)
+    .appendTo(gitDateContainer);
+
+
+
+    $("<span>")
+    .addClass("text-desc")
+    .html(project.desc)
+    .appendTo(textContainer);
+    
+    if (project.image2 != "nopic"){
+        projectImage.addClass("an1");
+        let  timer = setInterval(swap,5000)
+    }
+    function swap(){
+   
+        if (x == 1){
+            projectImage.attr("src", project.path + project.image2)
+        
+            projectImage.addClass("an2")
+            projectImage.removeClass("an1")
+            return x = 2;
+        }
+        if (x == 2){
+            projectImage.attr("src", project.path + project.image3)
+            projectImage.removeClass("an2")
+            projectImage.addClass("an3")
+       
+            return x = 3;
+        }
+        if (x == 3){
+            projectImage.attr("src", project.path + project.image)
+            projectImage.removeClass("an3")
+
+            projectImage.addClass("an1")
+            return x = 1;
+        }
+    }
+ 
+    
+   
+    
+}
 
 
 function getProduct(){
@@ -27,3 +96,5 @@ function getProduct(){
      }
 
 }
+
+
